225fb194d2fbe41965038ee3867a38a1bd7f9f55
vManav123 <vmanav1999@gmail.com>
2021-03-13 21:03:27 +0530

    Java Project of Sum of 3 number

---
+++ Java Project/src/Sum_of_3_number.java
@@ -0,0 +1,19 @@
+
+public class Sum_of_3_number {
+
+	static int x,y,z;
+	public Sum_of_3_number(int x,int y,int z) {
+		// TODO Auto-generated constructor stub
+		this.x=x;
+		this.y=y;
+		this.z=z;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Sum_of_3_number s = new Sum_of_3_number(10,12,15);
+		System.out.println("Sum of three number : "+(x+y+z));
+
+	}
+
+}
