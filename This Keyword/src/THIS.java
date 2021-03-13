aa591b65d46046e718ac00b96d309392fc2d544f
vManav123 <vmanav1999@gmail.com>
2021-03-13 20:13:47 +0530

    This Keyword

---
+++ This Keyword/src/THIS.java
@@ -0,0 +1,18 @@
+
+public class THIS {
+
+	private static int x;
+	public THIS(int x) {
+		// TODO Auto-generated constructor stub
+		this.x = x;
+	}
+
+	public static void main(String[] args) {
+
+		// TODO Auto-generated method stub
+
+		THIS T = new THIS(10);
+		System.out.println("hello, here is a this keyword  "+x);
+	}
+
+}
