ac1e1cd13c91e53c768cdfa476858ef321bc0205
rajatbisen <53312019+rajatbisen@users.noreply.github.com>
2021-03-15 00:07:29 +0530

    Update Sum_of_3_number.java

--- Java Project/src/Sum_of_3_number.java
+++ Java Project/src/Sum_of_3_number.java
@@ -1,19 +1,24 @@
+package rajatb;
 
-public class Sum_of_3_number {
+public class Sum_of_four_number {
 
-	static int x,y,z;
-	public Sum_of_3_number(int x,int y,int z) {
-		// TODO Auto-generated constructor stub
-		this.x=x;
-		this.y=y;
-		this.z=z;
-	}
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Sum_of_3_number s = new Sum_of_3_number(10,12,15);
-		System.out.println("Sum of three number : "+(x+y+z));
 
-	}
+		static int x,y,z,a;
+		public Sum_of_four_number(int x,int y,int z,int a) {
+			// TODO Auto-generated constructor stub
+			this.x=x;
+			this.y=y;
+			this.z=z;
+			this.a=a;
+		}
 
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+			Sum_of_four_number s = new Sum_of_four_number(10,12,15,17);
+			System.out.println("Sum of four number : "+(x+y+z+a));
+
+		}
+
+	
 }
