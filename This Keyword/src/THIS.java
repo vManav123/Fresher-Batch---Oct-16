5221c501d33135715deec3205293129770f2fa45
Manav Verma <38263736+vManav123@users.noreply.github.com>
2021-03-14 23:32:37 +0530

    Update THIS.java

--- This Keyword/src/THIS.java
+++ This Keyword/src/THIS.java
@@ -1,18 +1,18 @@
-
 public class THIS {
 
-	private static int x;
-	public THIS(int x) {
+	private static int x,y;
+	public THIS(int x,int y) {
 		// TODO Auto-generated constructor stub
 		this.x = x;
+		this.y = y;
 	}
 
 	public static void main(String[] args) {
 
 		// TODO Auto-generated method stub
 
-		THIS T = new THIS(10);
-		System.out.println("hello, here is a this keyword  "+x);
+		THIS T = new THIS(10,20);
+		System.out.println("hello, here is a this keyword  "+(x + y));
 	}
 
 }
